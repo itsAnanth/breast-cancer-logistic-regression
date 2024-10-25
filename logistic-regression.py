bfa9cd636e4d7695354d8da63d9abb8ab637dce3
itsAnanth <danonymousarmy@gmai.com>
2024-10-25 19:51:01 +0530

    feat: readability with comments

--- logistic-regression.py
+++ logistic-regression.py
@@ -4,26 +4,47 @@ import pickle as pk
 from data import getData
 
 
+"""
+    data is obtained from sklearn load_breast_cancer() module
+    data is normalized using sklearn StandardScaler normalizer
+"""
 X_train, X_test, y_train, y_test, features, labels = getData()
 
 
 class LogisticRegression:
     
     def __init__(self, learning_rate=0.1, epochs=1000):
+        
+        # setting hyperparameters
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights = None
         self.bias = None
         
+    # activation function used to introduce non linearity in the model
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
     
+    """
+        derivative of sigmoid to be used in gradient calculation
+        this is not explicity used in the code whatsoever since the derivative of sigmoid cancels out neatly
+        with the derivative of binary cross entropy cost function
+    """
     def sigmoid_derivative(self, z):
         return z * (1 - z)
     
+    """
+        binary cross entropy cost function
+        np.mean for the cost across entire samples
+    """
     def binary_cross_entropy(self, y_true, y_pred):
         return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
     
+    """
+        utility function to load weights instead of training the model from scratch
+        
+        note: the model must be trained atleast once to make use of this function
+    """
     def _load_parameters(self):
         try:
             print("Loading model parameters from params.pkl")
@@ -39,8 +60,12 @@ class LogisticRegression:
             print(f"An unexpected error occurred: {e}")
             return False
     
+    """
+        actual training process
+    """
     def fit(self, X: np.ndarray, y: np.ndarray, dump_weights=False, epoch_split=100, use_weights=False):
         
+        # if model has been trained once, use pretrained weights
         if (use_weights):
             if (self._load_parameters()):
                 print("Model parameters found, skipping training")
@@ -48,29 +73,44 @@ class LogisticRegression:
             else:
                 print("params.pkl does not exist, proceeding to training the model")
                         
+        # training atleast once
         num_samples, num_features = X.shape
         
         self.weights = np.zeros(shape=(num_features, 1))
         self.bias = 0
+        
+        # convert labels to column vector to prevent shape mismatch
         y = y.reshape(-1, 1)
         for epoch in range(self.epochs):
+            
+            # pre activation
             z = X @ self.weights + self.bias
+            # activated outputs
             y_pred = self.sigmoid(z)
+            
+            # error delta
             error = y_pred - y
-            cost = self.binary_cross_entropy(y, y_pred)
+            
+            
             
             if (epoch % epoch_split == 0):
+                # calculate cost at certain epoch split
+                cost = self.binary_cross_entropy(y, y_pred)
                 print(f"{epoch}/{self.epochs} | loss = {cost}")
             
+            # gradient calculation and parameter updation
             dw = (1 / num_samples) * (X.T @ error)
             db = (1 / num_samples) * np.sum(error)
             
             self.weights += -self.learning_rate * dw
             self.bias += -self.learning_rate * db
             
+            
+        # skip writing parameters to file if user deems so
         if not dump_weights:
             return
         
+        # else write to file
         with open('params.pkl', 'wb') as f:
             print("Dumping model parameters to params.pkl")
             params = {
@@ -79,12 +119,25 @@ class LogisticRegression:
             }
             pk.dump(params, f)
             f.close()
-        
+     
+    """
+        prediction function
+        predicted values are in a range 0 to 1 (Since sigmoid activation is used)
+        map this prediction into binary classes based on a threshold value
+        return the mapped values in integer format
+        1 - malignant
+        0 - benign
+    """   
     def predict(self, X, threshold=0.5):
         z = X @ self.weights + self.bias
         y_pred = self.sigmoid(z)
         return (y_pred > threshold).astype(int)
     
+    """
+        accuracy of model with a given input and label
+        
+        Note: this is not f1 score
+    """
     def accuracy(self, X, y):
         predictions: np.ndarray = self.predict(X)
         
